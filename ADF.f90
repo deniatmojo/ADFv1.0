f330f774288e711d26cdb870994fc5b9f84144a8
deniatmojoo-pc2 <deni.atmojo@ymail.com>
2024-02-03 12:06:55 +0700

    Pembuatan keterangan untuk file lokal i5

--- ADF.f90
+++ ADF.f90
@@ -1,1 +1,32 @@
-
+!=======================================================================
+! Program     : Analisis Ducted Fan
+! Nama		  : Deni Atmojo
+! NIM		  : 13619041
+! Deskripsi   : Program ini merupakan program sederhana yang digunakan
+!               untuk menghitung pengaruh dari distribusi sudut serang
+!               pada airfoil di ductedfan
+! Pembuat     : Deni Atmojo
+! Versi       : 1.0
+! Tanggal     : [3 Februari 2024]
+! Ini adalah file di lokal-Intel corei5
+!=======================================================================
+program ADF
+    implicit none
+    character(len=100) :: nama_lengkap, file_name
+    integer :: ios
+    character(len=100) :: dummy_line
+    
+    ! Header Program Exe
+    print *, '======================================================================='
+    print *, 'Program     = ADF - Analisis Ducted Fan'
+    print *, 'Deskripsi   = Program ini untuk menganalisis pengaruh distribusi sudut serang'
+    print *, '              pada airfoil ducted fan'
+    print *, 'Pembuat     = Deni Atmojo'
+    Print *, 'NIM         = 13619041'
+    Print *, 'Version     = 1.0'
+    Print *, 'Tanggal     = [3 Februari 2024]'
+    Print *, 'Pembaharuan = Belum ada pembaharuan'
+    print *, '======================================================================='
+    print *, ''
+    print *, ''
+    print *, ''
